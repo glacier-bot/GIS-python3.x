af8f9d8a24a27e97082a57401d2a371b5ea8c44f
D.Q.Fan <14037071+glacier-bot@user.noreply.gitee.com>
2025-12-25 02:57:55 +0000

    新增批量脚本
    Signed-off-by: D.Q.Fan <14037071+glacier-bot@user.noreply.gitee.com>

---
+++ GIS常用脚本/11多图层批量分镇导出cad/exports_tuban.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+# def change(x):
+#     return x.split(u"镇")[0][4:]+u"镇"
+
+import arcpy
+print(u"开始执行")
+
+
+def select_uniq(x):
+    return list(set(x))
+
+
+path = r"D:\测试.gdb"
+
+feature_class = [u"图层1"]
+
+# 过滤掉的镇名
+ex_name = []
+
+arcpy.env.workspace = path
+
+for s_feature in feature_class:
+    print(u"开始导出：", s_feature)
+    names = []
+    with arcpy.da.SearchCursor(s_feature, [u'镇名']) as cursor:
+        for row in cursor:
+            if row[0] not in ex_name:
+                names.append(row[0])
+
+    print(u"筛选镇名")
+    uniq_names = select_uniq(names)
+    print(u"制作图层")
+    lyr_name = u'selected_lyr_{}'.format(s_feature)
+    arcpy.MakeFeatureLayer_management(s_feature, lyr_name)
+    print(u"开始导出")
+    for name in uniq_names:
+        sql_query = u"镇名='{}'".format(name)
+        arcpy.SelectLayerByAttribute_management(
+            lyr_name, "NEW_SELECTION", sql_query)
+        dest_path = u"D:/成果/图形cad/{0}.dwg".format(
+            name)
+        seed_path = u"D:/成果/序号cad/{0}.dwg".format(
+            name)
+        arcpy.ExportCAD_conversion(
+            lyr_name, "DWG_R2000", dest_path, Seed_File=seed_path)
+
+print(u"Finished")
